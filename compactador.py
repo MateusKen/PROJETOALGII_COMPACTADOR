704eea6d1f74c3c8fb8123541e926cbc1309a977
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-09-14 10:44:49 -0300

    Create compactador.py

--- compactador.py
+++ compactador.py
@@ -2,3 +2,60 @@
 1º FUNÇÃO: VERIFICA SE UMA PALAVRA ESTÁ PRESENTE NO VETOR CASO ENCONTRE A PALAVRA, RETORNA O ÍNDICE, SENÃO RETORNA -1
 2º FUNÇÃO: COPIA DO INÍCIO DO VETOR TODAS AS PALAVRAS UMA POSIÇÃO A DIREITA
 '''
+arquivo_L = open('texto.txt', 'r')
+arquivo_E = open('compactado.txt', 'w')
+tam = len(arquivo_L.readlines())
+arquivo_L.seek(0)
+
+def conferePalavra(palavra, lista):
+    for i in range(len(lista)):
+        if lista[i] == '':
+            break
+        else:
+            if palavra == lista[i]:
+                return i
+    return -1
+
+def moveVetor(indice, palavra, lista):
+    tamFila = 0
+    for i in range(len(lista)):
+        if lista[i] == '':
+            tamFila = i
+            break
+    if indice == -1:
+        for i in range(tamFila, 0, -1):
+            lista[i] = lista[i-1]
+            lista[0] = palavra
+    else:
+        palavraRepetida = lista[indice]
+        for i in range(indice, 0, -1):
+            lista[i] = lista[i-1]            
+            lista[0] = palavra
+
+vetLinhas = ['']*tam #vetor com cada linha e suas palavras
+
+for i in range(tam): 
+    linha = arquivo_L.readline()
+    linha = linha.rstrip()
+    vetLinha = linha.split(' ')
+    vetLinhas[i] = vetLinha
+
+palavras = []*1000
+for i in range(10):
+    print(i)
+for i in range(len(vetLinhas)):
+    for j in range(len(vetLinhas[i])):
+        estar = conferePalavra(vetLinhas[i][j], palavras)
+        if estar == -1:
+            #insere palavra na lista de palavras
+            moveVetor(estar, vetLinhas[i][j], palavras)
+            #escreve no arquivo palavra
+            arquivo_E.write(vetLinhas[i][j])
+        else:
+            #muda a posicao da palavra repetida para 1ª posicao
+            moveVetor(estar, vetLinhas[i][j], palavras)
+            #escreve índice da palavra repetida
+            arquivo_E.write(conferePalavra(vetLinhas[i][j], palavras))
+
+arquivo_L.close()
+arquivo_E.close()
